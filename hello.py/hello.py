c36e6d447c9c4efbc52ef4985bfa021d4a4e0943
Parkjindream <fhfg09820@gmail.com>
2025-07-30 15:37:07 +0700

    use config file for app name

--- hello.py/hello.py
+++ hello.py/hello.py
@@ -1,11 +1,8 @@
+import config
+
 def say_hello(name):
-   print(f"Hello, {name}!")
+   print(f"Hello, {name} from {config.APP_NAME}!")
+# ... ส่วนที่เหลือเหมือนเดิม
 
-def greet_user():
-   name = input("Please enter your name: ")
-   say_hello(name)
-
-if __name__ == "__main__":
-   greet_user()
 
 

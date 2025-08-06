05da65e240b2eb7f732343c78a580a6a39ee5f3c
Parkjindream <fhfg09820@gmail.com>
2025-08-06 14:27:26 +0700

    Initial commit

--- config.py
+++
@@ -1,5 +0,0 @@
-APP_NAME = " My Say Hello App "
-VERSION = "1.0.0"
-
-
-
--- hello.py/hello.py
+++ hello.py/hello.py
@@ -2,7 +2,14 @@ import config
 
 def say_hello(name):
    print(f"Hello, {name} from {config.APP_NAME}!")
-# ... ส่วนที่เหลือเหมือนเดิม
+
+def greet_user():
+   name = input("Please enter your name: ")
+   say_hello(name)
+
+if __name__ == "__main__":
+   greet_user()
+
 
 
 
